1c5bfaf18c8de75763a343876ae1597d1b04f77d
Daniel Mardunovich <mardunovich@gmail.com>
2026-01-15 19:03:24 +0100

    everything generates correctly for real now!
    weights set by chatgpt

--- src/map/edges.js
+++ src/map/edges.js
@@ -9,66 +9,209 @@ export const EDGES = {
       "grass_empty",
       "grass_grass",
       "grass_flower",
-      
+
+      "water_connectsgrass_bl",
+      "water_connectsgrass_br",
+      "water_connectsgrass_b",
+
+      "river_connectsgrass_bl",
+      "river_connectsgrass_br",
+      "river_connectsgrass_tb",
+
+      "river_connectsgrass_tbl",
+      "river_connectsgrass_tbr",
+      "river_connectsgrass_brl",
     ],
     down: [
       "grass_empty",
       "grass_grass",
-      "grass_flower"
+      "grass_flower",
+
+      "water_connectsgrass_tl",
+      "water_connectsgrass_tr",
+      "water_connectsgrass_t",
+
+      "river_connectsgrass_tl",
+      "river_connectsgrass_tr",
+      "river_connectsgrass_tb",
+
+      "river_connectsgrass_trl",
+      "river_connectsgrass_tbr",
+      "river_connectsgrass_tbl",
     ],
     left: [
       "grass_empty",
       "grass_grass",
-      "grass_flower"
+      "grass_flower",
+
+      "water_connectsgrass_br",
+      "water_connectsgrass_tr",
+      "water_connectsgrass_r",
+
+      "river_connectsgrass_br",
+      "river_connectsgrass_tr",
+      "river_connectsgrass_lr",
+
+      "river_connectsgrass_trl",
+      "river_connectsgrass_brl",
+      "river_connectsgrass_tbr",
     ],
     right: [
       "grass_empty",
       "grass_grass",
-      "grass_flower"
+      "grass_flower",
+
+      "water_connectsgrass_bl",
+      "water_connectsgrass_tl",
+      "water_connectsgrass_l",
+
+      "river_connectsgrass_bl",
+      "river_connectsgrass_tl",
+      "river_connectsgrass_lr",
+
+      "river_connectsgrass_trl",
+      "river_connectsgrass_brl",
+      "river_connectsgrass_tbl",
     ]
   },
   grass_grass: {
     up: [
       "grass_empty",
       "grass_grass",
-      "grass_flower"
+      "grass_flower",
+
+      "water_connectsgrass_bl",
+      "water_connectsgrass_br",
+      "water_connectsgrass_b",
+
+      "river_connectsgrass_bl",
+      "river_connectsgrass_br",
+      "river_connectsgrass_tb",
+
+      "river_connectsgrass_tbl",
+      "river_connectsgrass_tbr",
+      "river_connectsgrass_brl",
     ],
     down: [
       "grass_empty",
       "grass_grass",
-      "grass_flower"
+      "grass_flower",
+
+      "water_connectsgrass_tl",
+      "water_connectsgrass_tr",
+      "water_connectsgrass_t",
+
+      "river_connectsgrass_tl",
+      "river_connectsgrass_tr",
+      "river_connectsgrass_tb",
+
+      "river_connectsgrass_trl",
+      "river_connectsgrass_tbr",
+      "river_connectsgrass_tbl",
     ],
     left: [
       "grass_empty",
       "grass_grass",
-      "grass_flower"
+      "grass_flower",
+
+      "water_connectsgrass_br",
+      "water_connectsgrass_tr",
+      "water_connectsgrass_r",
+
+      "river_connectsgrass_br",
+      "river_connectsgrass_tr",
+      "river_connectsgrass_lr",
+
+      "river_connectsgrass_trl",
+      "river_connectsgrass_brl",
+      "river_connectsgrass_tbr",
     ],
     right: [
       "grass_empty",
       "grass_grass",
-      "grass_flower"
+      "grass_flower",
+
+      "water_connectsgrass_bl",
+      "water_connectsgrass_tl",
+      "water_connectsgrass_l",
+
+      "river_connectsgrass_bl",
+      "river_connectsgrass_tl",
+      "river_connectsgrass_lr",
+
+      "river_connectsgrass_trl",
+      "river_connectsgrass_brl",
+      "river_connectsgrass_tbl",
     ]
   },
   grass_flower: {
     up: [
       "grass_empty",
       "grass_grass",
-      "grass_flower"
+      "grass_flower",
+
+      "water_connectsgrass_bl",
+      "water_connectsgrass_br",
+      "water_connectsgrass_b",
+
+      "river_connectsgrass_bl",
+      "river_connectsgrass_br",
+      "river_connectsgrass_tb",
+
+      "river_connectsgrass_tbl",
+      "river_connectsgrass_tbr",
+      "river_connectsgrass_brl",
     ],
     down: [
       "grass_empty",
       "grass_grass",
-      "grass_flower"
+      "grass_flower",
+
+      "water_connectsgrass_tl",
+      "water_connectsgrass_tr",
+      "water_connectsgrass_t",
+
+      "river_connectsgrass_tl",
+      "river_connectsgrass_tr",
+      "river_connectsgrass_tb",
+
+      "river_connectsgrass_trl",
+      "river_connectsgrass_tbr",
+      "river_connectsgrass_tbl",
     ],
     left: [
       "grass_empty",
       "grass_grass",
-      "grass_flower"
+      "grass_flower",
+
+      "water_connectsgrass_br",
+      "water_connectsgrass_tr",
+      "water_connectsgrass_r",
+
+      "river_connectsgrass_br",
+      "river_connectsgrass_tr",
+      "river_connectsgrass_lr",
+
+      "river_connectsgrass_trl",
+      "river_connectsgrass_brl",
+      "river_connectsgrass_tbr",
     ],
     right: [
       "grass_empty",
       "grass_grass",
-      "grass_flower"
+      "grass_flower",
+
+      "water_connectsgrass_bl",
+      "water_connectsgrass_tl",
+      "water_connectsgrass_l",
+
+      "river_connectsgrass_bl",
+      "river_connectsgrass_tl",
+      "river_connectsgrass_lr",
+
+      "river_connectsgrass_trl",
+      "river_connectsgrass_brl",
+      "river_connectsgrass_tbl",
     ]
   },
 
@@ -76,194 +219,607 @@ export const EDGES = {
   // Water Tiles L
   //==========================
   water_connectsgrass_tl: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+
+    ],
+    down: 
+    [
+      "water_connectsgrass_l",
+    ],
+    left: 
+    [
+      "water_connectsriver_t"
+    ],
+    right: 
+    [
+      "water_connectsgrass_t",
+      "water_connectsriver_t",
+    ]
   },
   water_connectsgrass_bl: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "water_connectsgrass_l",
+      "water_connectsriver_l",
+    ],
+    down: 
+    [
+      
+    ],
+    left: 
+    [
+      
+    ],
+    right: 
+    [
+      "water_connectsgrass_b",
+      "water_connectsriver_b",
+    ]
   },
   water_connectsgrass_tl_corner: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "water_connectsgrass_tl",
+      "water_connectsgrass_l",
+
+      "water_connectsriver_l",
+
+      "water_connectsgrass_bl_corner",
+    ],
+    down: 
+    [
+      "water_center"
+    ],
+    left: 
+    [
+      "water_connectsgrass_tl",
+      "water_connectsriver_t",
+
+      "water_connectsgrass_t",
+
+      "water_connectsgrass_tr_corner",
+    ],
+    right: 
+    [
+      "water_center"
+    ]
   },
   water_connectsgrass_bl_corner: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "water_center"
+    ],
+    down: 
+    [
+      "water_connectsgrass_bl",
+      "water_connectsgrass_l",
+
+      "water_connectsriver_l",
+
+      "water_connectsgrass_tl_corner",
+    ],
+    left: 
+    [
+      "water_connectsgrass_bl",
+      "water_connectsgrass_b",
+
+      "water_connectsriver_b",
+
+      "water_connectsgrass_br_corner",
+    ],
+    right: 
+    [
+      "water_center",
+    ]
   },
 
   //==========================
   // Water Tiles R
   //==========================
   water_connectsgrass_tr: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      
+    ],
+    down: 
+    [
+      "water_connectsgrass_r",
+      "water_connectsriver_r",
+    ],
+    left: 
+    [
+      "water_connectsgrass_t",
+      "water_connectsriver_t",
+    ],
+    right: 
+    [
+
+    ]
   },
   water_connectsgrass_br: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "water_connectsgrass_r",
+      "water_connectsriver_t",
+    ],
+    down: 
+    [
+
+    ],
+    left: 
+    [
+      "water_connectsgrass_b",
+      "water_connectsriver_t",
+    ],
+    right: 
+    [
+
+    ]
   },
   water_connectsgrass_tr_corner: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "water_connectsgrass_tr",
+      "water_connectsgrass_r",
+
+      "water_connectsriver_r",
+
+      "water_connectsgrass_br_corner",
+    ],
+    down: 
+    [
+      "water_center"
+    ],
+    left: 
+    [
+      "water_center",
+    ],
+    right: 
+    [
+      "water_connectsgrass_tr",
+      "water_connectsgrass_t",
+
+      "water_connectsriver_t",
+
+      "water_connectsgrass_tl_corner",
+    ]
   },
   water_connectsgrass_br_corner: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "water_center"
+    ],
+    down: 
+    [
+      "water_connectsgrass_br",
+      "water_connectsgrass_r",
+
+      "water_connectsriver_r",
+
+      "water_connectsgrass_tr_corner",
+    ],
+    left: 
+    [
+      "water_center",
+    ],
+    right: 
+    [
+      "water_connectsgrass_br",
+      "water_connectsgrass_b",
+
+      "water_connectsriver_b",
+
+      "water_connectsgrass_bl_corner",
+    ]
   },
 
   //==========================
   // Water Tiles C
   //==========================
   water_center: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "water_center",
+
+      "water_connectsgrass_t",
+      "water_connectsriver_t",
+
+      "water_connectsgrass_tl_corner",
+      "water_connectsgrass_tr_corner",
+
+      "water_connectsriver_t"
+
+
+    ],
+    down: 
+    [
+      "water_center",
+
+      "water_connectsgrass_b",
+      "water_connectsriver_b",
+
+      "water_connectsgrass_br_corner",
+      "water_connectsgrass_bl_corner",
+
+      "water_connectsriver_b"
+    ],
+    left: 
+    [
+      "water_center",
+
+      "water_connectsgrass_l",
+      "water_connectsriver_l",
+
+      "water_connectsgrass_tl_corner",
+      "water_connectsgrass_bl_corner",
+
+      "water_connectsriver_l",
+    ],
+    right: 
+    [
+      "water_center",
+
+      "water_connectsgrass_r",
+      "water_connectsriver_r",
+
+      "water_connectsgrass_tr_corner",
+      "water_connectsgrass_br_corner",
+
+      "water_connectsriver_r",
+    ]
   },
   water_connectsgrass_l: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "water_connectsgrass_l",
+      "water_connectsriver_l",
+    ],
+    down: 
+    [
+      "water_connectsgrass_l",
+      "water_connectsriver_l",
+    ],
+    left: 
+    [
+
+    ],
+    right: 
+    [
+
+    ]
   },
   water_connectsgrass_r: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "water_connectsgrass_r",
+      "water_connectsriver_r",
+    ],
+    down: 
+    [
+      "water_connectsgrass_r",
+      "water_connectsriver_r",
+    ],
+    left: 
+    [
+
+    ],
+    right: 
+    [
+
+    ]
   },
   water_connectsgrass_t: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+
+    ],
+    down: 
+    [
+
+    ],
+    left: 
+    [
+      "water_connectsgrass_t",
+      "water_connectsriver_t",
+    ],
+    right: 
+    [
+      "water_connectsgrass_t",
+      "water_connectsriver_t",
+    ]
   },
   water_connectsgrass_b: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+
+    ],
+    down: 
+    [
+
+    ],
+    left: 
+    [
+      "water_connectsgrass_b",
+      "water_connectsriver_b",
+    ],
+    right: 
+    [
+      "water_connectsgrass_b",
+      "water_connectsriver_b",
+    ]
   },
 
   //==========================
   //River to water connections
   //==========================
   water_connectsriver_l: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+
+    ],
+    down: 
+    [
+
+    ],
+    left: 
+    [
+      "river_connectsgrass_tb"
+    ],
+    right: 
+    [
+
+    ]
   },
   water_connectsriver_r: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+
+    ],
+    down: 
+    [
+
+    ],
+    left: 
+    [
+
+    ],
+    right: 
+    [
+      "river_connectsgrass_tb"
+    ]
   },
   water_connectsriver_t: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "river_connectsgrass_lr"
+    ],
+    down: 
+    [
+
+    ],
+    left: 
+    [
+
+    ],
+    right: 
+    [
+
+    ]
   },
   water_connectsriver_b: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+
+    ],
+    down: 
+    [
+      "river_connectsgrass_lr"
+    ],
+    left: 
+    [
+
+    ],
+    right: 
+    [
+
+    ]
   },
 
   //==========================
   //River Tiles L
   //==========================
   river_connectsgrass_bl: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "river_connectsgrass_lr",
+    ],
+    down: 
+    [
+
+    ],
+    left: 
+    [
+
+    ],
+    right: 
+    [
+      "river_connectsgrass_tb",
+    ]
   },
   river_connectsgrass_tl: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+
+    ],
+    down: 
+    [
+      "river_connectsgrass_lr",
+    ],
+    left: 
+    [
+
+    ],
+    right: 
+    [
+      "river_connectsgrass_tb",
+    ]
   },
 
   //==========================
   //River Tiles R
   //==========================
   river_connectsgrass_tr: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+
+    ],
+    down: 
+    [
+      "river_connectsgrass_lr",
+    ],
+    left: 
+    [
+      "river_connectsgrass_tb",
+    ],
+    right: 
+    [
+
+    ]
   },
   river_connectsgrass_br: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "river_connectsgrass_lr"
+    ],
+    down: 
+    [
+
+    ],
+    left: 
+    [
+      "river_connectsgrass_tb"
+    ],
+    right: 
+    [
+
+    ]
   },
 
   //==========================
   //River Tiles C
   //==========================
   river_connectsgrass_tb: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+
+    ],
+    down: 
+    [
+
+    ],
+    left: 
+    [
+      "river_connectsgrass_tb",
+    ],
+    right: 
+    [
+      "river_connectsgrass_tb",
+    ]
   },
   river_connectsgrass_lr: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "river_connectsgrass_lr",
+    ],
+    down: 
+    [
+      "river_connectsgrass_lr",
+    ],
+    left: 
+    [
+
+    ],
+    right: 
+    [
+
+    ]
   },
 
   //==========================
   //River Endings
   //==========================
   river_connectsgrass_tbl: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+
+    ],
+    down: 
+    [
+
+    ],
+    left: 
+    [
+
+    ],
+    right: 
+    [
+      "river_connectsgrass_tb",
+    ]
   },
   river_connectsgrass_tbr: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+
+    ],
+    down: 
+    [
+
+    ],
+    left: 
+    [
+      "river_connectsgrass_tb",
+    ],
+    right: 
+    [
+
+    ]
   },
   river_connectsgrass_trl: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+
+    ],
+    down: 
+    [
+      "river_connectsgrass_lr",
+    ],
+    left: 
+    [
+
+    ],
+    right: 
+    [
+
+    ]
   },
   river_connectsgrass_brl: {
-    up: [],
-    down: [],
-    left: [],
-    right: []
+    up: 
+    [
+      "river_connectsgrass_lr",
+    ],
+    down: 
+    [
+
+    ],
+    left: 
+    [
+
+    ],
+    right: 
+    [
+
+    ]
   }
 };
 
--- src/map/tiles.js
+++ src/map/tiles.js
@@ -2,51 +2,51 @@ export const TILE_SIZE = 16;
 
 export const TILES = [
   //Full grass tiles
-  { id: "grass_empty", sx: 0, sy: 0, weight: 75 },
-  { id: "grass_grass", sx: 1, sy: 0, weight: 50 },
-  { id: "grass_flower", sx: 2, sy: 0, weight: 6 },
+  { id: "grass_empty", sx: 0, sy: 0, weight: 120 },
+  { id: "grass_grass", sx: 1, sy: 0, weight: 90 },
+  { id: "grass_flower", sx: 2, sy: 0, weight: 12 },
 
   // Water Tiles L
-  { id: "water_connectsgrass_tl", sx: 0, sy: 1, weight: 1 },
-  { id: "water_connectsgrass_bl", sx: 0, sy: 3, weight: 1 },
-  { id: "water_connectsgrass_tl_corner", sx: 0, sy: 5, weight: 1 },
-  { id: "water_connectsgrass_bl_corner", sx: 3, sy: 5, weight: 1 },
+  { id: "water_connectsgrass_tl", sx: 0, sy: 1, weight: 3 },
+  { id: "water_connectsgrass_bl", sx: 0, sy: 3, weight: 3 },
+  { id: "water_connectsgrass_tl_corner", sx: 0, sy: 5, weight: 2 },
+  { id: "water_connectsgrass_bl_corner", sx: 3, sy: 5, weight: 2 },
 
   // Water Tiles R
-  { id: "water_connectsgrass_tr", sx: 2, sy: 1, weight: 1 },
-  { id: "water_connectsgrass_br", sx: 2, sy: 3, weight: 1 },
-  { id: "water_connectsgrass_tr_corner", sx: 1, sy: 5, weight: 1 },
-  { id: "water_connectsgrass_br_corner", sx: 2, sy: 5, weight: 1 },
+  { id: "water_connectsgrass_tr", sx: 2, sy: 1, weight: 3 },
+  { id: "water_connectsgrass_br", sx: 2, sy: 3, weight: 3 },
+  { id: "water_connectsgrass_tr_corner", sx: 1, sy: 5, weight: 2 },
+  { id: "water_connectsgrass_br_corner", sx: 2, sy: 5, weight: 2 },
 
   // Water Tiles C
-  { id: "water_center", sx: 1, sy: 2, weight: 1 },
-  { id: "water_connectsgrass_l", sx: 0, sy: 2, weight: 1 },
-  { id: "water_connectsgrass_r", sx: 2, sy: 2, weight: 1 },
-  { id: "water_connectsgrass_t", sx: 1, sy: 1, weight: 1 },
-  { id: "water_connectsgrass_b", sx: 1, sy: 3, weight: 1 },
+  { id: "water_center", sx: 1, sy: 2, weight: 12 },
+  { id: "water_connectsgrass_l", sx: 0, sy: 2, weight: 5 },
+  { id: "water_connectsgrass_r", sx: 2, sy: 2, weight: 5 },
+  { id: "water_connectsgrass_t", sx: 1, sy: 1, weight: 5 },
+  { id: "water_connectsgrass_b", sx: 1, sy: 3, weight: 5 },
 
   //River to water connections
-  { id: "water_connectsriver_l", sx: 2, sy: 4, weight: 1 },
-  { id: "water_connectsriver_r", sx: 0, sy: 4, weight: 1 },
-  { id: "water_connectsriver_t", sx: 3, sy: 2, weight: 1 },
-  { id: "water_connectsriver_b", sx: 3, sy: 4, weight: 1 },
+  { id: "water_connectsriver_l", sx: 2, sy: 4, weight: 0.8 },
+  { id: "water_connectsriver_r", sx: 0, sy: 4, weight: 0.8 },
+  { id: "water_connectsriver_b", sx: 3, sy: 2, weight: 0.8 },
+  { id: "water_connectsriver_t", sx: 3, sy: 4, weight: 0.8 },
 
   //River Tiles L
-  { id: "river_connectsgrass_bl", sx: 3, sy: 1, weight: 1 },
-  { id: "river_connectsgrass_tl", sx: 3, sy: 0, weight: 1 },
+  { id: "river_connectsgrass_bl", sx: 3, sy: 1, weight: 2 },
+  { id: "river_connectsgrass_tl", sx: 3, sy: 0, weight: 2 },
 
   //River Tiles R
-  { id: "river_connectsgrass_tr", sx: 4, sy: 0, weight: 1 },
-  { id: "river_connectsgrass_br", sx: 4, sy: 1, weight: 1 },
+  { id: "river_connectsgrass_tr", sx: 4, sy: 0, weight: 2 },
+  { id: "river_connectsgrass_br", sx: 4, sy: 1, weight: 2 },
 
   //River Tiles C
-  { id: "river_connectsgrass_tb", sx: 1, sy: 4, weight:1 },
-  { id: "river_connectsgrass_lr", sx: 3, sy: 3, weight: 1},
+  { id: "river_connectsgrass_tb", sx: 1, sy: 4, weight: 6 },
+  { id: "river_connectsgrass_lr", sx: 3, sy: 3, weight: 6 },
   
    //River Endings
-  { id: "river_connectsgrass_tbl", sx: 6, sy: 1, weight: 1 },
-  { id: "river_connectsgrass_tbr", sx: 6, sy: 0, weight: 1 },
-  { id: "river_connectsgrass_trl", sx: 5, sy: 0, weight: 1 },
-  { id: "river_connectsgrass_brl", sx: 5, sy: 1, weight: 1 },
+  { id: "river_connectsgrass_tbl", sx: 6, sy: 1, weight: 0.05 },
+  { id: "river_connectsgrass_tbr", sx: 6, sy: 0, weight: 0.05 },
+  { id: "river_connectsgrass_trl", sx: 5, sy: 0, weight: 0.05 },
+  { id: "river_connectsgrass_brl", sx: 5, sy: 1, weight: 0.05 },
 
 ];
